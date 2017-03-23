d71b3b0c1b569567790b09d9bce28f815f246cf9
Petra Karman <karmanpetra@gmail.com>
2017-03-23 17:36:14 +0100

    functions and arrays 13-16

---
+++ week-02/day-04/src/Colors.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Petra on 2017. 03. 23..
+ */
+public class Colors {
+  public static void main(String[] args) {
+    String[][] colors = new String[3][];
+    String[] green = {"lime,", "forest green,", "olive,", "pale green,", "spring green"};
+    String[] red = {"orange red,", "red,", "tomato"};
+    String[] pink = {"orchid,", "violet,", "pink,", "hot pink"};
+    colors[0] = green;
+    colors[1] = red;
+    colors[2] = pink;
+
+    for (int i = 0; i < colors.length; i++) {
+      for (int j = 0; j < colors[i].length; j++) {
+
+        System.out.print(" " + colors[i][j]);
+
+      }
+      System.out.println();
+    }
+  }
+}
+
+// - Create a two dimensional array
+//   which can contain the different shades of specified colors
+// - In `colors[0]` store the shades of green:
+//   `"lime", "forest green", "olive", "pale green", "spring green"`
+// - In `colors[1]` store the shades of red:
+//   `"orange red", "red", "tomato"`
+// - In `colors[2]` store the shades of pink:
+//   `"orchid", "violet", "pink", "hot pink"`
---
+++ week-02/day-04/src/DoubleItems.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Petra on 2017. 03. 23..
+ */
+public class DoubleItems {
+  public static void main(String[] args) {
+    int[] ag = {3, 4, 5, 6, 7};
+    for (int i = 0; i < ag.length; i++) {
+      System.out.println(2*(ag [i]));
+    }
+  }
+
+}
+
+
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
