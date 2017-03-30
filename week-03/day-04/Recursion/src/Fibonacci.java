28fac1e6d9d83a9795678ba5934735c0dfa98217
Petra Karman <karmanpetra@gmail.com>
2017-03-30 15:56:26 +0200

    Fibonacci corrected version

--- week-03/day-04/Recursion/src/Fibonacci.java
+++ week-03/day-04/Recursion/src/Fibonacci.java
@@ -6,11 +6,11 @@ public class Fibonacci {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter an integer:");
     int userInput = scanner.nextInt();
-    System.out.println(fibonacci(userInput));
+    System.out.println(fibonacci(userInput-1));
   }
 
   private static int fibonacci(int n) {
-    if (n == 0) {
+    if (n <= 0) {
       return 0;
     } else if (n == 1) {
       return 1;
