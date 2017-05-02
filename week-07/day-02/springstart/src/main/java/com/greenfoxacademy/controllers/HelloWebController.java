20e5a5a0429d261276fbb98191436d5a73a5c7a0
Petra Karman <karmanpetra@gmail.com>
2017-05-02 17:07:00 +0200

    web greet counter app

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
@@ -3,13 +3,17 @@ package com.greenfoxacademy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  AtomicLong counter = new AtomicLong();
   @RequestMapping("/web/greeting")
   public String greeting(Model model) {
     model.addAttribute("name", " World");
+    model.addAttribute("counter", counter.addAndGet(1));
     return "greeting";
   }
 }
