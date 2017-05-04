8373951f7f5007da74cffbe4c07921ca3383e2e1
Petra Karman <karmanpetra@gmail.com>
2017-05-04 12:04:53 +0200

    Hello World

---
+++ week-07/day-04/HellooWorld/src/main/java/com/greenfoxacademy/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication implements CommandLineRunner{
+	@Autowired
+	HelloWorld hello;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		hello.setMessage("Hello World!");
+		hello.getMessage();
+	}
+}
